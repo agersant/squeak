da68fceb9eb282532c8f2d2d17579d4177887987
Antoine Gersant <antoine.gersant@lesforges.org>
2022-12-16 22:46:22 -0800

    Added test for nested subscribe calls

--- tests/test_delegate.rs
+++ tests/test_delegate.rs
@@ -20,6 +20,31 @@ fn delegate_executes_callbacks() {
 }
 
 #[test]
+fn can_subscribe_during_callback() {
+    let d = Arc::new(ReentrantMutex::new(Delegate::new()));
+    let outer_count = Arc::new(Mutex::new(RefCell::new(0)));
+    let inner_count = Arc::new(Mutex::new(RefCell::new(0)));
+    {
+        let d_clone = d.clone();
+        let outer_count_clone = outer_count.clone();
+        let inner_count_clone = inner_count.clone();
+        d.lock().subscribe(move |_| {
+            *outer_count_clone.lock().borrow_mut() += 1;
+            let inner_count_clone_clone = inner_count_clone.clone();
+            d_clone.lock().subscribe(move |_| {
+                *inner_count_clone_clone.lock().borrow_mut() += 1;
+                Response::CancelSubscription
+            });
+            Response::CancelSubscription
+        });
+        d.lock().notify();
+        d.lock().notify();
+    }
+    assert_eq!(*outer_count.lock().borrow(), 1);
+    assert_eq!(*inner_count.lock().borrow(), 1);
+}
+
+#[test]
 fn delegate_does_not_execute_unsubscribed_callbacks() {
     let mut call_count = 0;
     {
